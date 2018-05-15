22ea60157047313f5eee91317c4e62f4ec5d9e10
USER-20161201OC\Administrator <376952372@qq.com>
2018-05-15 09:27:16 +0800

    fix: set the 'Move to' option's value to be '' and value 'none' can only be the last option 设置第一选项为空，则option取值为 ‘none’时，指向列表最后一个选项

--- src/Book.js
+++ src/Book.js
@@ -22,7 +22,7 @@ return(
     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks !== undefined ? book.imageLinks.thumbnail: ''})` }}></div>
      <div className="book-shelf-changer">
      <select value={book.shelf} onChange={(e) => this.changeShelf(e.target.value)}>
-      <option value="none" disabled>Move to...</option>
+      <option value="" disabled>Move to...</option>
       <option value="currentlyReading">Currently Reading</option>
       <option value="wantToRead">Want to Read</option>
        <option value="read">Read</option>
